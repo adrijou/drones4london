047f401cf5f8c06ef7c34b778d910d800335fa90
Adrian Salas <asalgav@gmail.com>
2016-03-02 20:14:37 +0000

    First commit application: Archetype generated by maven

---
+++ src/main/java/com/propero/drones/App.java
@@ -0,0 +1,13 @@
+package com.propero.drones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
